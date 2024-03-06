6b93aea0043ef60d15c51f7364d2a4f349bce4a9
lephuchung <lephuchung003@gmail.com>
2024-03-07 00:36:30 +0700

    video7

--- src/App.js
+++ src/App.js
@@ -4,25 +4,31 @@ import Nav from './Component/Nav/Nav';
 
 const App = () => {
   let name = 'Lê Hưng';
-  let number = 2003;
-  let obj = {
-    firstName: "Noris",
-    lastName: "Nora"
-  };
-  let link = "https://www.facebook.com/le.phuchung.25040601";
+  // let number = 2003;
+  // let obj = {
+  //   firstName: "Noris",
+  //   lastName: "Nora"
+  // };
+  // let link = "https://www.facebook.com/le.phuchung.25040601";
+  const handleClickButton = (event) => {
+    console.log('click btn', event.target.value);
+  }
+
   return (
     <div className="App">
-      {console.log('check obj: ', obj)}
+      {/* {console.log('check obj: ', obj)} */}
       <header className="App-header">
         <Nav />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Lê Phúc Hưng hello world,
         </p>
-        <h3>{name} - {number}</h3>
+        <input type='text' value={name} onClick={(event) => handleClickButton(event)} />
+        <button type='button' onClick={(event) => handleClickButton(event)}>Click me  </button>
+        {/* <h3>{name} - {number}</h3>
         <h4>{obj.firstName} {obj.lastName}</h4>
         <p>{JSON.stringify(obj)}</p>
-        <a href={link}>My facebook</a>
+        <a href={link}>My facebook</a> */}
       </header>
     </div>
   );
