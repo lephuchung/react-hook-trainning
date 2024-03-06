a6a42250b9fa78f318c04737aed702588d444c25
lephuchung <lephuchung003@gmail.com>
2024-03-06 22:35:14 +0700

    video4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 const App = () => {
+  let name = 'Lê Hưng';
+  let number = 2003;
+  let obj = {
+    firstName: "Noris",
+    lastName: "Nora"
+  };
+  let link = "https://www.facebook.com/le.phuchung.25040601";
   return (
     <div className="App">
+      {console.log('check obj: ', obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Lê Phúc Hưng hello world
+          Lê Phúc Hưng hello world,
         </p>
+        <h3>{name} - {number}</h3>
+        <h4>{obj.firstName} {obj.lastName}</h4>
+        <p>{JSON.stringify(obj)}</p>
+        <a href={link}>My facebook</a>
       </header>
     </div>
   );
