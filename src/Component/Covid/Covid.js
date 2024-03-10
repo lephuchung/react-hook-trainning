951eb265d0753ff45a1208ceb30fc5e72da6c629
lephuchung <lephuchung003@gmail.com>
2024-03-10 23:41:07 +0700

    video17

--- src/Component/Covid/Covid.js
+++ src/Component/Covid/Covid.js
@@ -3,19 +3,25 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Covid = () => {
-    const [dataCovid, setDataCovid] = useState([])
+    const [dataCovid, setDataCovid] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const fetchData = async () => {
-        let res = await axios.get(`https://covid-api.com/api/reports?date=2021-03-14&q=China%20Beijing&iso=CHN&region_name=China`);
+        let data_url = 'https://covid-api.com/api/reports?date=2021-03-14&q=China%20Beijing&iso=CHN&region_name=China'
+        let res = await axios.get(data_url);
         let data = res && res.data && res.data.data ? res.data.data : [];
         console.log(data);
         setDataCovid(data);
+        setLoading(false);
     }
 
     useEffect(() => {
-        fetchData();
-        console.log('check data: ', dataCovid);
-    }, [])
+        setTimeout(async () => {
+            fetchData();
+            console.log('check data: ', dataCovid);
+        }, 1500)
+
+    }, []);
     return (
         <div className='covid-container'>
             <hr></hr>
@@ -25,6 +31,8 @@ const Covid = () => {
             <table>
                 <thead>
                     <tr>
+                        <th>No.</th>
+
                         <th>Provine</th>
                         <th>Confirmed</th>
                         <th>Active</th>
@@ -33,17 +41,25 @@ const Covid = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {dataCovid.map((item, index) => {
-                        return (
-                            <tr>
-                                <td>{item.region.province}</td>
-                                <td>{item.confirmed}</td>
-                                <td>{item.active}</td>
-                                <td>{item.deaths}</td>
-                                <td>{item.recovered}</td>
-                            </tr>
-                        )
-                    })
+                    {loading === false && dataCovid && dataCovid.length > 0 &&
+                        dataCovid.map((item, index) => {
+                            return (item.region.province !== 'Unknown' ?
+                                <tr key={item.id}>
+                                    <td>{index + 1}</td>
+                                    <td>{item.region.province}</td>
+                                    <td>{item.confirmed}</td>
+                                    <td>{item.active}</td>
+                                    <td>{item.deaths}</td>
+                                    <td>{item.recovered}</td>
+                                </tr>
+                                : <div><p>  </p></div>
+                            )
+                        })
+                    }
+                    {loading === true &&
+                        <tr>
+                            <td colSpan={6} style={{ 'textAlign': 'center' }}>loading</td>
+                        </tr>
                     }
                 </tbody>
 
